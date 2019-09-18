dd0631014e50dc21318a7463caf5b20ba9873720
yantao006 <yantao006@gmail.com>
2019-09-18 11:54:42 +0800

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -1,3 +1,12 @@
 
 for i in $(get_instance_by_service shoubai-gr-tucheng.SUPERPAGE.bj); do ssh -o "StrictHostKeyChecking no" $i "grep call_generator /home/work/shoubai-gr/log/gr.log.wf " ; done > /tmp/22 &
 
+# git 技巧
+
+## 
+
+## 查看历史commit_id的改动
+### 首先使用git blame 查看对应的代码改动负责人、改动时间，改动的commit_id
+### 然后通过 git diff ${commit_id}^! 查看对应改动diff
+git diff ${commit_id}^!
+
